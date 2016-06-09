9a8a3263fd10a143e22dde19efd40f12bc63bc92
Brent Colson <bacolson@gmail.com>
2016-06-08 22:39:13 -0400

    set up express server, added middlewear, added dev dependencies to package.json

--- server/app.js
+++ server/app.js
@@ -1,1 +1,23 @@
-app.js
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+
+//Server logger
+app.use(morgan('dev'));
+
+app.use('/', express.static(path.resolve(__dirname, '../client')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+//Serves up your public/client folder
+
+
+//Router
+app.use('/', ideas);
+
+
+app.listen(8000, function(){
+  console.log('server is now listening on port 8000.');
+});
